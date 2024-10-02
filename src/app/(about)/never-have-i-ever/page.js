cc79c92d907448fccbb7c85704b2d7e26e073521
Ananya Tripathi <ananyatripathi@TCPLP-IN-0886.local>
2024-10-02 15:52:56 +0530

    Added Never Have I ever game

--- src/app/(about)/never-have-i-ever/page.js
+++ src/app/(about)/never-have-i-ever/page.js
@@ -2,19 +2,15 @@
 
 import { useState } from "react";
 
-const TruthDare = () => {
+const NeverHaveIEver = () => {
   const [ageGroup, setAgeGroup] = useState("21-30");
   const [playingWith, setPlayingWith] = useState("friends");
   const [userPrompt, setUserPrompt] = useState("");
   const [rounds, setRounds] = useState(5);
   const [gender, setGender] = useState("male & female");
 
-  const [truths, setTruths] = useState([]);
-  const [dares, setDares] = useState([]);
+  const [nhieQuestions, setNhieQuestions] = useState([]);
 
-  const [rotation, setRotation] = useState(0);
-  const [selectedPlayer, setSelectedPlayer] = useState("");
-  const [playerNames, setPlayerNames] = useState([]);
   const [rotationCount, setRotationCount] = useState(0);
   const [errorMessage, setErrorMessage] = useState("");
 
@@ -22,66 +18,35 @@ const TruthDare = () => {
   const [apiError, setApiError] = useState("")
   const [apiLoading, setApiLoading] = useState(false)
 
-  const rotateBottle = () => {
-    setSuggestion("")
-    if (rotationCount < rounds) {
-      const randomRotation = Math.floor(Math.random() * 360) + 1080; 
-      setRotation(randomRotation);
-      const randomIndex = Math.floor(Math.random() * playerNames.length);
-      setSelectedPlayer(playerNames[randomIndex]);
-      setRotationCount(rotationCount + 1);
-      setErrorMessage("");
-    } else {
-      setErrorMessage(`Oops...You have reached the maximum number of ${rounds} rotations!`);
-    }
-  };
-
-  const addPlayerInput = () => {
-    setPlayerNames([...playerNames, ""]);
-  };
-
-  const updatePlayerName = (index, value) => {
-    const newPlayerNames = [...playerNames];
-    newPlayerNames[index] = value;
-    setPlayerNames(newPlayerNames);
-  };
-
   const onSubmitBtn = async () => {
     setApiLoading(true);
-    if(playerNames.length > 0){
-      const res = await fetch(`https://ai-games-self.vercel.app/api/tds/answer`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          "age_group": ageGroup,
-          "gender": gender,
-          "playing_with": playingWith,
-          "rounds": rounds,
-          "user_prompt": userPrompt,
-        }),
-      });
-    
-      if (!res.ok) {
-        setApiError("Oops...some issue occurred. Please refresh the page and try again.");
-        console.log("Error refreshing access token");
-        setApiLoading(false);
-      } else {
-        const data = await res.json();
-        setTruths(data[0]["truth"]);
-        setDares(data[0]["dare"]);
-        setApiLoading(false);
-  
-        window.scrollTo({
-          top: document.body.scrollHeight,
-          behavior: "smooth", 
-        });
-      }
+    const res = await fetch(`https://ai-games-self.vercel.app/api/tds/answer-nhie`, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        "age_group": ageGroup,
+        "gender": gender,
+        "playing_with": playingWith,
+        "rounds": rounds,
+        "user_prompt": userPrompt,
+    }),
+    });
 
-    }
-    else{
-      setApiError("Oops..Please add users to start the game");
+    if (!res.ok) {
+    setApiError("Oops...some issue occurred. Please refresh the page and try again.");
+    console.log("Error refreshing access token");
+    setApiLoading(false);
+    } else {
+    const data = await res.json();
+    setNhieQuestions(data[0]["suggestions"]);
+    setApiLoading(false);
+
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth", 
+    });
     }
   };
 
@@ -89,15 +54,31 @@ const TruthDare = () => {
     window.location.reload(false)
   }
 
+  const onViewQuestions = () => {
+    setSuggestion(nhieQuestions[rotationCount])
+    setRotationCount(rotationCount + 1)
+  }
+
+  const onNextClick = () =>{
+    if(rotationCount < rounds){
+        setSuggestion(nhieQuestions[rotationCount])
+        setRotationCount(rotationCount + 1)
+    }
+    else{
+        setErrorMessage(`Oops...You have reached the maximum number of ${rounds} rounds!`)
+    }
+  }
+
   return (
     <section className="w-full mt-16 sm:mt-24 md:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32 flex flex-col items-center justify-center">
       {/* Heading and description */}
+      {console.log("Hii", rounds, rotationCount)}
       <div className="text-center mb-10">
         <h1 className="text-4xl sm:text-5xl font-bold text-[#FABC3F]">
-          Truth, Dare and AI Surprises
+            Never Have I Ever with AI's Twist: Discover What You’ve Never Done
         </h1>
         <p className="text-lg sm:text-xl text-gray-100 mt-4 dark:text-white">
-          Get ready to take your favorite party game to the next level with AI-powered truths and dares! Just give us a prompt, and we'll suggest fun, personalized challenges for your group.
+            Dive into a world of AI-crafted challenges! ‘Never Have I Ever’ has never been this unpredictable. Get ready for wild confessions and fun revelations tailored just for you! Just give us a prompt, and we'll suggest fun, personalized questions for your group.
         </p>
       </div>
       <form className="w-full max-w-2xl bg-white p-8 rounded-lg shadow-md" onSubmit={onSubmitBtn}>
@@ -178,33 +159,6 @@ const TruthDare = () => {
             />
             <div className="text-center mt-2">{rounds} Rounds</div>
           </div>
-
-          {/* Player Names */}
-          <div className="col-span-2 mt-4">
-            <label className="block text-gray-700 font-medium mb-2">Player Names</label>
-
-            <div className="flex flex-wrap gap-4">
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 flex-grow">
-                {playerNames.map((name, index) => (
-                  <input
-                    key={index}
-                    type="text"
-                    value={name}
-                    onChange={(e) => updatePlayerName(index, e.target.value)}
-                    className="w-full border border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none"
-                    placeholder={`Player ${index + 1} Name`}
-                  />
-                ))}
-                <button
-                  type="button"
-                  onClick={addPlayerInput}
-                  className="py-2 px-4 bg-green-500 text-white font-semibold rounded-lg hover:bg-green-600 transition duration-300 ease-in-out h-fit self-start"
-                >
-                  + Add Player
-                </button>
-              </div>
-            </div>
-          </div>
         </div>
 
         {apiError && (
@@ -213,7 +167,7 @@ const TruthDare = () => {
           </div>
         )}
 
-        {truths.length > 0 && (
+        {nhieQuestions.length > 0 && (
           <div className="mt-4 text-green-500 font-semibold">
             Yayyy! Game started, scroll down and have fun!
           </div>
@@ -222,36 +176,19 @@ const TruthDare = () => {
         <div className="mt-8">
           <button
             type="button"
-            onClick={truths.length > 0 ? onEndClick : onSubmitBtn}
+            onClick={nhieQuestions.length > 0 ? onEndClick : onSubmitBtn}
             disabled={apiLoading}
-            className={`w-full py-3 px-6 ${truths.length > 0 ? 'bg-red-500' : 'bg-[#FABC3F]'} text-white font-semibold rounded-lg hover:${truths.length > 0 ? 'bg-red-600' : 'bg-[#FCCD2A]'} transition duration-300 ease-in-out`}
+            className={`w-full py-3 px-6 ${nhieQuestions.length > 0 ? 'bg-red-500' : 'bg-[#FABC3F]'} text-white font-semibold rounded-lg hover:${nhieQuestions.length > 0 ? 'bg-red-600' : 'bg-[#FCCD2A]'} transition duration-300 ease-in-out`}
           >
-            {apiLoading ? "Loading..." : truths.length > 0 ? "End Game" : "Start Game"}
+            {apiLoading ? "Loading..." : nhieQuestions.length > 0 ? "End Game" : "Start Game"}
           </button>
         </div>
       </form>
 
-      {truths.length !=  0 ?(
+      {nhieQuestions.length != 0 ? (
         <div>
       {/* Bottle and Rotate Button */}
       <div className="mt-16 flex flex-col items-center justify-center">
-        <div className="relative">
-          <img
-            id="bottle"
-            src="/bottle.png"
-            alt="Spinning Bottle"
-            className="w-32 h-72 transition-transform duration-1000 ease-out"
-            style={{ transform: `rotate(${rotation}deg)` }} 
-          />
-        </div>
-
-        <button
-          className="mt-6 py-2 px-4 bg-[#FABC3F] text-white font-semibold rounded-lg hover:bg-[#FCCD2A] transition duration-300 ease-in-out"
-          onClick={rotateBottle}
-        >
-          Rotate
-        </button>
-
         <p className="text-xl mt-4">
             <span className="bg-gradient-to-r from-pink-500 to-orange-500 bg-clip-text text-transparent font-extrabold">
               Round No. {rotationCount}!
@@ -264,25 +201,18 @@ const TruthDare = () => {
           </div>
         )}
 
-        {selectedPlayer && (
-          <div className="mt-6 text-2xl font-bold text-gray-700 dark:text-white">
-            {selectedPlayer} was chosen!
-          </div>
-        )}
-
-        {console.log("rounds", rounds)}
-
-        {/* Truth and Dare buttons */}
-        {selectedPlayer && (
-          <div className="flex space-x-6 mt-6">
-            <button onClick={()=>setSuggestion(truths[rotationCount-1])} className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition duration-300 ease-in-out">
-              Truth
+        <div className="flex space-x-6 mt-6">
+            {rotationCount == 0 ? (
+                <button onClick={onViewQuestions} className="py-2 px-4 bg-red-500 text-white font-semibold rounded-lg hover:bg-red-600 transition duration-300 ease-in-out">
+                View Questions
             </button>
-            <button onClick={()=>setSuggestion(dares[rotationCount-1])} className="py-2 px-4 bg-red-500 text-white font-semibold rounded-lg hover:bg-red-600 transition duration-300 ease-in-out">
-              Dare
-            </button>
-          </div>
-        )}
+            ):
+            <button onClick={onNextClick} className="py-2 px-4 bg-red-500 text-white font-semibold rounded-lg hover:bg-red-600 transition duration-300 ease-in-out">
+                Next Question
+            </button>}
+            
+        </div>
+
       </div>
 
       {/* Display the suggestion */}
@@ -301,17 +231,16 @@ const TruthDare = () => {
           </p>
 
           <p className="mt-4 text-lg font-medium text-gray-700">
-            Make sure to be honest if it's Truth and bold if it's a Dare!
+            Be ready to reveal the truth or face a surprise! No holding back!
           </p>
         </div>
       )}
         </div>
-
-      ):null}
+      ): null}
 
 
     </section>
   );
 };
 
-export default TruthDare;
+export default NeverHaveIEver;
--- src/components/Home/RecentPosts.js
+++ src/components/Home/RecentPosts.js
@@ -3,20 +3,37 @@ import Link from "next/link";
 import React from "react";
 import BlogLayoutThree from "../Blog/BlogLayoutThree";
 
-const sortedBlogs = [{
-  title: 'Truth, Dare, and AI Surprises: AI crafts exciting truths and dares tailored just for you!',
-  description: 'Experience truth and dares like never before with AI-generated questions and challenges tailored just for you!',
-  image: {
-    filePath: '../public/truth-or-dare.jpg',
-    relativeFilePath: '../../public/blogs/emile-perron-xrVDYZRGdw4-unsplash.jpg',
-    format: 'jpeg',
-    height: 1080,
-    width: 1920,
-    aspectRatio: 1.7777777777777777,
-    blurhashDataUrl: 'data:image/jpeg;base64,/9j/2wBDAAoHBwgHBgoICAgLCgoLDhgQDg0NDh0VFhEYIx8lJCIfIiEmKzcvJik0KSEiMEExNDk7Pj4+JS5ESUM8SDc9Pjv/2wBDAQoLCw4NDhwQEBw7KCIoOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozv/wAARCAAIAAgDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAP/xAAcEAABBQADAAAAAAAAAAAAAAACAAEDBAYRIUH/xAAVAQEBAAAAAAAAAAAAAAAAAAAAAf/EABYRAAMAAAAAAAAAAAAAAAAAAAABEf/aAAwDAQACEQMRAD8Agewigzxx17BVrtcXGMm7YibzlERWIVn/2Q=='
+const sortedBlogs = [
+  {
+    title: 'Truth, Dare, and AI Surprises: AI crafts exciting truths and dares tailored just for you!',
+    description: 'Experience truth and dares like never before with AI-generated questions and challenges tailored just for you!',
+    image: {
+      filePath: '../public/truth-or-dare.jpg',
+      relativeFilePath: '../../public/blogs/emile-perron-xrVDYZRGdw4-unsplash.jpg',
+      format: 'jpeg',
+      height: 1080,
+      width: 1920,
+      aspectRatio: 1.7777777777777777,
+      blurhashDataUrl: 'data:image/jpeg;base64,/9j/2wBDAAoHBwgHBgoICAgLCgoLDhgQDg0NDh0VFhEYIx8lJCIfIiEmKzcvJik0KSEiMEExNDk7Pj4+JS5ESUM8SDc9Pjv/2wBDAQoLCw4NDhwQEBw7KCIoOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozv/wAARCAAIAAgDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAP/xAAcEAABBQADAAAAAAAAAAAAAAACAAEDBAYRIUH/xAAVAQEBAAAAAAAAAAAAAAAAAAAAAf/EABYRAAMAAAAAAAAAAAAAAAAAAAABEf/aAAwDAQACEQMRAD8Agewigzxx17BVrtcXGMm7YibzlERWIVn/2Q=='
+    },
+    url: '/truth-or-dare'
   },
-  url: '/truth-or-dare'
-}]
+  {
+    title: "Never Have I Ever with AI's Twist: Discover What You’ve Never Done & prepare for unexpected!",
+    description: "Dive into a world of AI-crafted challenges! ‘Never Have I Ever’ has never been this unpredictable. Get ready for wild confessions and fun revelations tailored just for you!",
+    image: {
+      filePath: '../public/never_have_i_ever.webp',
+      relativeFilePath: '../../public/blogs/emile-perron-xrVDYZRGdw4-unsplash.jpg',
+      format: 'webp',
+      height: 1080,
+      width: 1920,
+      aspectRatio: 1.7777777777777777,
+      blurhashDataUrl: 'data:image/jpeg;base64,/9j/2wBDAAoHBwgHBgoICAgLCgoLDhgQDg0NDh0VFhEYIx8lJCIfIiEmKzcvJik0KSEiMEExNDk7Pj4+JS5ESUM8SDc9Pjv/2wBDAQoLCw4NDhwQEBw7KCIoOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozv/wAARCAAIAAgDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAP/xAAcEAABBQADAAAAAAAAAAAAAAACAAEDBAYRIUH/xAAVAQEBAAAAAAAAAAAAAAAAAAAAAf/EABYRAAMAAAAAAAAAAAAAAAAAAAABEf/aAAwDAQACEQMRAD8Agewigzxx17BVrtcXGMm7YibzlERWIVn/2Q=='
+    },
+    url: '/never-have-i-ever'
+  }
+
+]
 
 
 const RecentPosts = () => {
